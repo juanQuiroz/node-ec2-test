39618ebab08e1ed64cb9068e83ba43aeb10c0128
JuanQuiroz <32399659+juanQuiroz@users.noreply.github.com>
2021-03-19 18:23:54 -0500

    port 8080
    fix - bugs

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const app = express();
 app.get("/", (req, res) => res.send("Hello world con express v2"));
 
 app.listen(8080);
-console.log("Servidor ejecutandose en el servidor 8080
+console.log("Servidor ejecutandose en el servidor 8080")
