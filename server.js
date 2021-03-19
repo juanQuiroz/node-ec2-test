9742abae971aeb7f78c4a2dfd66016d9c9f98615
JuanQuiroz <32399659+juanQuiroz@users.noreply.github.com>
2021-03-19 17:49:31 -0500

    v2 added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello world con express"));
+app.get("/", (req, res) => res.send("Hello world con express v2"));
 
 app.listen(3000);
 console.log("Servidor ejecutandose en el servidor 3000");
